faee093b0dbccba8cdffc0ec5fb254ce2a0149bd
Braily Guzman <brailyg@outlook.com>
2024-10-10 11:02:35 -0400

    Completed code challenge

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 //write your code here
+
+const reverseString = (str) => {
+  const indexLength = str.length - 1;
+  let reversed = '';
+
+  for (let i = indexLength; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+};
+
+const reverseZigZagString = (str) => {
+  const indexLength = str.length - 1;
+  let reversed = '';
+
+  for (let i = indexLength; i >= 0; i -= 2) {
+    reversed += str[i].toLowerCase() + str[i - 1].toUpperCase();
+  }
+
+  return reversed;
+};
+
+// TESTS
+// console.log(reverseString('Braily')); // Passed
+// console.log(reverseZigZagString('Braily')); // Passed
